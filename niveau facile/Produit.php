1c503b0ada3d5dd7e73ea5e34339cc15910ec857
makhfi03 <ghita.makhfi.21@gmail.com>
2026-02-10 14:51:42 +0100

    ajout de classe produit

---
+++ niveau facile/Produit.php
@@ -0,0 +1,18 @@
+<!-- Gestion de Produits -->
+
+<?php
+
+class Produit{
+    private $nom;
+    private $prix;
+
+    public function __construct($nom, $prix)
+    {
+        $this->nom = $nom;
+        $this->prix = $prix;
+    }
+
+    public function affichage(){
+        echo "le prix de" . $this->nom . "est:" . $this->prix . "DH";
+    }
+}
