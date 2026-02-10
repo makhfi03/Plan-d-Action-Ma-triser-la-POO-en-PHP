a564e08fcf11215cbcdd2363013d382b6eaf8443
makhfi03 <ghita.makhfi.21@gmail.com>
2026-02-10 12:10:25 +0100

    classe voiture

---
+++ niveau facile/Voiture.php
@@ -0,0 +1,18 @@
+<!-- Plan d'Action : Maîtriser la POO en PHP
+Niveau Facile : Les Fondations -->
+
+<?php
+class Voiture{
+     public $marque;
+    public $couleur;
+    public $vitesse;
+    public function accelerer(){
+        $this->vitesse += 10;
+    }
+
+    public function afficherInfos(){
+        echo "Marque : " . $this->marque . "<br>";
+        echo "Couleur : " . $this->couleur . "<br>";
+        echo "Vitesse : " . $this->vitesse . "<br>";
+    }
+}
